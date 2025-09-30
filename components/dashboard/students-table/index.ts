31506fe97feaa36417b996ffa88abcf75bb1aa40
Christian Vargas <vargas.christian@correounivalle.edu.co>
2025-09-29 23:27:25 -0500

    Agrega componente AdminDashboard con integración de StudentsTable para visualizar estudiantes #47

---
+++ components/dashboard/students-table/index.ts
@@ -0,0 +1,4 @@
+export { StudentsTable } from './students-table'
+export { DeleteStudentsDialog } from './delete-students-dialog'
+export { StudentAvatar } from './student-avatar'
+export type { Student, Grade } from './types'
